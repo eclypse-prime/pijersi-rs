2dcc976e0876fd4a8377c3eb51bb7138551652d4
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-04-07 00:39:10 +0200

    refactor: added basic example (perft) in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod board;
+mod logic;
+mod lookup;
+
+use std::time::Instant;
+
+use board::Board;
+use logic::movegen::perft;
 
 fn main() {
-    let mut board: board::Board = board::Board::new();
+    let mut board: Board = Board::new();
     board.init();
-    board.print();
+
+    let start = Instant::now();
+    for _ in 0..5 {
+        let results = perft(&board.cells, board.current_player, 4);
+        println!("result {results}");
+    }
+    let elapsed = start.elapsed();
+    println!("Rust time {:.2?}", elapsed);
+
 }
