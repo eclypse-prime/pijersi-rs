808d6c1e176fb75da7af230894a81f3cd0c88da1
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-04-06 14:59:14 +0200

    refactor: used constants for clarity

--- src/logic/rules.rs
+++ src/logic/rules.rs
@@ -1,11 +1,11 @@
-use super::{COLOUR_MASK, TYPE_MASK};
+use super::{COLOUR_MASK, TYPE_MASK, TYPE_PAPER, TYPE_ROCK, TYPE_SCISSORS, TYPE_WISE};
 
 pub fn can_take(attacker: u8, target: u8) -> bool {
     let attacker_type: u8 = attacker & TYPE_MASK;
     let target_type: u8 = target & TYPE_MASK;
-    (attacker_type == 0 && target_type == 4)
-        || (attacker_type == 4 && target_type == 8)
-        || (attacker_type == 8 && target_type == 0)
+    (attacker_type == TYPE_SCISSORS && target_type == TYPE_PAPER)
+        || (attacker_type == TYPE_PAPER && target_type == TYPE_ROCK)
+        || (attacker_type == TYPE_ROCK && target_type == TYPE_SCISSORS)
 }
 
 pub fn can_move1(cells: [u8; 45], moving_piece: u8, index_end: usize) -> bool {
@@ -40,10 +40,10 @@ pub fn can_stack(cells: [u8; 45], moving_piece: u8, index_end: usize) -> bool
     // If the end cell is not empty
     // If the target piece and the moving piece are the same colour
     // If the end piece is not a stack
-    if (cells[index_end] != 0) && ((cells[index_end] & 2) == (moving_piece & 2)) && (cells[index_end] < 16)
+    if (cells[index_end] != 0) && ((cells[index_end] & COLOUR_MASK) == (moving_piece & COLOUR_MASK)) && (cells[index_end] < 16)
     {
         // If the upper piece is Wise and the target piece is not Wise
-        if (moving_piece & 12) == 12 && (cells[index_end] & 12) != 12
+        if (moving_piece & TYPE_MASK) == TYPE_WISE && (cells[index_end] & TYPE_MASK) != TYPE_WISE
         {
             return false;
         }
