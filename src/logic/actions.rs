a3062a5a59b2ed2654ca5c4a7d3fab5d1041a37e
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-04-07 00:30:24 +0200

    refactor: moved parameter order for consistency

--- src/logic/actions.rs
+++ src/logic/actions.rs
@@ -2,7 +2,7 @@ use super::{
     CELL_EMPTY, COLOUR_MASK, HALF_PIECE_WIDTH, INDEX_MASK, INDEX_NULL, INDEX_WIDTH, TOP_MASK,
 };
 
-pub fn do_move(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
+pub fn do_move(cells: &mut [u8; 45], index_start: usize, index_end: usize) {
     if index_start != index_end {
         // Move the piece to the target cell
         cells[index_end] = cells[index_start];
@@ -12,7 +12,7 @@ pub fn do_move(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
     }
 }
 
-pub fn do_stack(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
+pub fn do_stack(cells: &mut [u8; 45], index_start: usize, index_end: usize) {
     let piece_start: u8 = cells[index_start];
     let piece_end: u8 = cells[index_end];
 
@@ -23,7 +23,7 @@ pub fn do_stack(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
     cells[index_end] = (piece_start & TOP_MASK) + (piece_end << HALF_PIECE_WIDTH);
 }
 
-pub fn do_unstack(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
+pub fn do_unstack(cells: &mut [u8; 45], index_start: usize, index_end: usize) {
     let piece_start: u8 = cells[index_start];
 
     // Leave the bottom piece in the starting cell
@@ -35,10 +35,10 @@ pub fn do_unstack(index_start: usize, index_end: usize, cells: &mut [u8; 45]) {
     cells[index_end] = piece_start & TOP_MASK;
 }
 
-pub fn play_action(action: u64, cells: &mut [u8; 45]) {
-    let index_start: usize = (action & INDEX_MASK) as usize;
+pub fn play_action(cells: &mut [u8; 45], action: u64) {
     let index_mid: usize = ((action >> INDEX_WIDTH) & INDEX_MASK) as usize;
     let index_end: usize = ((action >> (2 * INDEX_WIDTH)) & INDEX_MASK) as usize;
+    let index_start: usize = (action & INDEX_MASK) as usize;
 
     if index_start == INDEX_NULL {
         return;
@@ -50,7 +50,7 @@ pub fn play_action(action: u64, cells: &mut [u8; 45]) {
         // If there is no intermediate move
         if index_mid == INDEX_NULL {
             // Simple move
-            do_move(index_start, index_end, cells);
+            do_move(cells, index_start, index_end);
         } else {
             let piece_mid: u8 = cells[index_mid];
             let piece_end: u8 = cells[index_end];
@@ -59,21 +59,25 @@ pub fn play_action(action: u64, cells: &mut [u8; 45]) {
                 && (piece_mid & COLOUR_MASK) == (piece_start & COLOUR_MASK)
                 && (index_start != index_mid)
             {
-                do_stack(index_start, index_mid, cells);
-                do_move(index_mid, index_end, cells);
+                do_stack(cells, index_start, index_mid);
+                do_move(cells, index_mid, index_end);
             }
             // The piece at the end coordinates is an ally : move and stack
             else if piece_end != CELL_EMPTY
                 && (piece_end & COLOUR_MASK) == (piece_start & COLOUR_MASK)
             {
-                do_move(index_start, index_mid, cells);
-                do_stack(index_mid, index_end, cells);
+                do_move(cells, index_start, index_mid);
+                do_stack(cells, index_mid, index_end);
             }
             // The end coordinates contain an enemy or no piece : move and unstack
             else {
-                do_move(index_start, index_mid, cells);
-                do_unstack(index_mid, index_end, cells)
+                do_move(cells, index_start, index_mid);
+                do_unstack(cells, index_mid, index_end)
             }
         }
     }
+}
+
+pub fn copy_cells(origin: &[u8; 45], target: &mut [u8; 45]) -> () {
+    target.copy_from_slice(origin);
 }
