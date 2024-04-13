74947bdfb9c6f12ab2752474bed51509381415fd
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-04-13 03:05:56 +0200

    refactor: modified main to test alphabeta

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::time::Instant;
 
 use pijersi_rs::board::Board;
-use pijersi_rs::logic::perft::perft;
+use pijersi_rs::logic::actions::play_action;
+use pijersi_rs::logic::rules::is_action_win;
+use pijersi_rs::logic::translate::action_to_string;
+use pijersi_rs::search::alphabeta::search;
 
 fn main() {
     let mut board: Board = Board::new();
     board.init();
 
-    let start = Instant::now();
-    for _ in 0..10 {
-        let results = perft(&board.cells, board.current_player, 4);
-        println!("result {results}");
+    loop {
+        let action =
+            search(&board.cells, board.current_player, 4);
+        println!("{}", action_to_string(&board.cells, action));
+        if is_action_win(&board.cells, action) {
+            play_action(&mut board.cells, action);
+            board.print();
+            println!("win");
+            break;
+        } else {
+            play_action(&mut board.cells, action);
+            board.current_player = 1 - board.current_player;
+            board.print();
+        }
     }
-    let elapsed = start.elapsed();
-    println!("Rust time {:.3?}", elapsed);
 }
