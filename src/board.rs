637ba0c45bdef4d02da083e20edda47b7f49fad7
Arthur Liu <arthur.liu.lsh@protonmail.com>
2024-05-12 23:51:32 +0200

    fix: put ignore on code block that made doctest fail

--- src/board.rs
+++ src/board.rs
@@ -3,7 +3,7 @@
 //! A board is represented as a [u8; 45] array.
 //!
 //! Its cells are indexed as such:
-//! ```
+//! ```ignore
 //!   0   1   2   3   4   5
 //! 6   7   8   9   10  11  12
 //!   13  14  15  16  17  18
