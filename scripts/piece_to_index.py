16a03910e105376746e98c1ac9c6453a545ae3ff
Eclypse-Prime <eclypse.prime@gmail.com>
2025-04-22 23:01:41 +0200

    #99 fix: fixed PIECE_TO_INDEX table to take into account the new piece representation

--- scripts/piece_to_index.py
+++ scripts/piece_to_index.py
@@ -1,12 +1,12 @@
 """Generates the code that uses a lookup table to convert a piece's u8 representation to an index so it can be used by other lookup tables."""
 
-conversion = {'S':1, 'P': 5, 'R': 9, 'W': 13, 's': 3, 'p': 7, 'r': 11, 'w': 15}
+conversion = {'S':0, 'P': 1, 'R': 2, 'W': 3, 's': 4, 'p': 5, 'r': 6, 'w': 7}
 
 def piece_to_int(piece: str):
     if len(piece) == 1:
-        return conversion[piece]
+        return conversion[piece] + 8
     else:
-        return conversion[piece[0]] + conversion[piece[1]]*16
+        return conversion[piece[0]] + 8 + conversion[piece[1]]*16 + 128
 
 pieces = []
 
